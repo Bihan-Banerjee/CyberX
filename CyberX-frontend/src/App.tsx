ad520ce5240b3a23962bd4c3da5175b816b04280
Bihan-Banerjee <bihanbanerjee26@gmail.com>
2025-06-24 13:04:15 +0530

    Initial FE commit

---
+++ CyberX-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="p-4 text-center">
+      <h1 className="text-2xl font-bold">CyberRangeX</h1>
+      <p className="text-gray-500">Your cybersecurity simulation lab starts here.</p>
+    </div>
+  );
+}
+
+export default App;
